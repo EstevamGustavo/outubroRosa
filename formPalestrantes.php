f1e21679d4ae02e16269fa41189b6dc76f282bae
Estevam_Gustavo <gustavo.estevam@outlook.com>
2018-10-14 15:57:11 -0300

    form

---
+++ formPalestrantes.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+
+<head>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+    <script>
+        $(function(){ 
+                  $("#header").load("header.html");
+                  $("#footer").load("footer.html"); 
+         });
+   </script>
+    <title>Meu Form</title>
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <meta charset="utf-8">
+
+</head>
+
+<body>
+    <div id = "header"></div>
+    <div class = "cadastro">
+        <form name="formPalestrante" method="post" action="#">
+       
+       
+       </form>    
+
+    </div> 
+   
+    
+
+
+</body>
+
+
+
+</html>
